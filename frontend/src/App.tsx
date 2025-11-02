a28c3fa5a9f171b5907067941ff287022a4b3826
fakeplastique <ihor.bondar.work@gmail.com>
2025-11-02 11:36:29 +0200

    adding browser router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,88 +1,14 @@
-import { useState, useEffect } from 'react';
-import {
-  Container,
-  AppBar,
-  Toolbar,
-  Typography,
-  Button,
-} from '@mui/material';
-import { Add as AddIcon } from '@mui/icons-material';
-import VehicleList from './components/VehicleList';
-import VehicleDialog from './components/VehicleDialog';
-import { getVehicles, createVehicle, updateVehicle, deleteVehicle } from './api/vehicles';
-import { Vehicle, VehiclesFilter, VehicleFormData } from './types/vehicle';
-import { VehicleFilterLayout } from './components/VehicleFilterLayout';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import VehiclesPage from './pages/VehiclesPage';
 
 function App() {
-  const [vehicles, setVehicles] = useState<Vehicle[]>([]);
-  const [dialogOpen, setDialogOpen] = useState(false);
-  const [selectedVehicle, setSelectedVehicle] = useState<Vehicle | null>(null);
-  const [filter, setFilter] = useState<VehiclesFilter>({});
-
-
-  const loadVehicles = async () => {
-    const data = await getVehicles(filter);
-    setVehicles(data);
-  };
-
-  useEffect(() => {
-    loadVehicles();
-  }, [filter]);
-
-  const handleCreate = () => {
-    setSelectedVehicle(null);
-    setDialogOpen(true);
-  };
-
-  const handleEdit = (vehicle: Vehicle) => {
-    setSelectedVehicle(vehicle);
-    setDialogOpen(true);
-  };
-
-  const handleDelete = async (id: string) => {
-    await deleteVehicle(id);
-    loadVehicles();
-  };
-
-  const handleSave = async (data: VehicleFormData) => {
-    if (selectedVehicle) {
-      await updateVehicle(selectedVehicle.id, data);
-    } else {
-      await createVehicle(data);
-    }
-    setDialogOpen(false);
-    loadVehicles();
-  };
-
   return (
-    <>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Autopark Management System
-          </Typography>
-          <Button color="inherit" startIcon={<AddIcon />} onClick={handleCreate}>
-            Add Vehicle
-          </Button>
-        </Toolbar>
-      </AppBar>
-
-      <Container maxWidth="lg" sx={{ mt: 4 }}>
-        <VehicleFilterLayout filterChange={setFilter}/>
-        <VehicleList
-          vehicles={vehicles}
-          onEdit={handleEdit}
-          onDelete={handleDelete}
-        />
-      </Container>
-
-      <VehicleDialog
-        open={dialogOpen}
-        vehicle={selectedVehicle}
-        onClose={() => setDialogOpen(false)}
-        onSave={handleSave}
-      />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/vehicles" />} />
+        <Route path="/vehicles" element={<VehiclesPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
